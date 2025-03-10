7ba043c90543c66d1d5e790a9ac2bee32b1b4b68
Piotr Dudziak <dudziak.piotrek@gmail.com>
2025-03-10 21:23:00 +0100

    Delete Task1/Kolejka_Zdarzen.java

--- Task1/Kolejka_Zdarzen.java
+++
@@ -1,8 +0,0 @@
-package Ruch_Miejski;
-
-public
-interface Kolejka_Zdarzen {
-    public abstract void wstaw(Zdarzenia zdarzenie);
-    public abstract Zdarzenia pobierzPierwszy();
-    public abstract boolean czyPusta();
-}
